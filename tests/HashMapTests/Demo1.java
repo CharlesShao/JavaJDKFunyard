d752f0ceac174ae98019f3e4fdeae0e7f829e321
CharlesShao <charles196127@icloud.com>
2020-03-26 21:23:53 +0800

    create jdk funyard

---
+++ tests/HashMapTests/Demo1.java
@@ -0,0 +1,14 @@
+package HashMapTests;
+
+import java.util.HashMap;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        HashMap<String, Double> hashMap = new HashMap<String, Double>();
+        hashMap.put("key1", 0.1);
+        hashMap.put("key2", 0.2);
+        hashMap.put("key3", 0.3);
+        hashMap.put("key4", 0.4);
+        hashMap.put("key5", 0.5);
+    }
+}
